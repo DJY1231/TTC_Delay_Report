2bc1dcc5a9839216897fa2ec6a1ba0f79812cef7
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-09-26 22:20:23 -0400

    Update and rename 04-test_analysis_data.R to 03-test_data.R

---
+++ scripts/03-test_data.R
@@ -0,0 +1,16 @@
+#### Preamble ####
+# Purpose: Tests the code
+# Author: Dong Jun Yoon
+# Date: 26 September 2024 
+# Contact: dongjun.yoon@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(tidyverse)
+library(kableExtra)
+library(dplyr)
+library(knitr)
+library(ggplot2)
+
+
+#### Test data ####
