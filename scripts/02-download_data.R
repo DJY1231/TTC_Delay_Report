0c22ee3a160537296adfcff33e48809c3931bb17
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-09-26 22:17:35 -0400

    Update 02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,23 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data
+# Author: Dong Jun Yoon
+# Date: 26 September 2024 
+# Contact: dongjun.yoon@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(kableExtra)
+library(dplyr)
+library(knitr)
+library(ggplot2)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
 write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
-         
