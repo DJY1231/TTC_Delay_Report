ff14fd17ceffc444c20b3e22b61cbb359672a133
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-09-26 22:20:43 -0400

    Update and rename 05-exploratory_data_analysis.R to 04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,16 +1,17 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models
+# Author: Dong Jun Yoon
+# Date: 26 September 2024 
+# Contact: dongjun.yoon@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(kableExtra)
+library(dplyr)
+library(knitr)
+library(ggplot2)
 
 #### Read data ####
 analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
