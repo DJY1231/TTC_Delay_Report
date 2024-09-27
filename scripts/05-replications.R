ca1ab9076fe268989ebd76fbecc1cebeb8ae76dc
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-09-26 22:21:06 -0400

    Update and rename 06-model_data.R to 05-replications.R

---
+++ scripts/05-replications.R
@@ -0,0 +1,16 @@
+#### Preamble ####
+# Purpose: Replicated graphs from
+# Author: Dong Jun Yoon
+# Date: 26 September 2024 
+# Contact: dongjun.yoon@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(tidyverse)
+library(kableExtra)
+library(dplyr)
+library(knitr)
+library(ggplot2)
+
+#### Load data ####
+
